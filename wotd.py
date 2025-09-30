a5665644083810ed5e350d9d9adae55111acb8aa
pnhwill <wharrington2016@my.fit.edu>
2025-09-29 21:58:07 -0400

    initiate commit. requirements. gitignore

---
+++ wotd.py
@@ -0,0 +1,13 @@
+# Word of the Day
+# Will Harrington
+# 29 Sep 2025
+
+from PyDictionary import PyDictionary
+from pathlib import Path
+
+INPUT_FILE = Path("/Users/will/Library/Mobile Documents/iCloud~md~obsidian/Documents/Random/words.md")
+
+if __name__ == "__main__":
+
+    dictionary = PyDictionary()
+
