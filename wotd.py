62fda6f6aae48c13362c200ab93243bb058553b7
pnhwill <wharrington2016@my.fit.edu>
2025-09-30 00:05:34 -0400

    working main functionality: choose random word from list and print with meanings

--- wotd.py
+++ wotd.py
@@ -2,12 +2,34 @@
 # Will Harrington
 # 29 Sep 2025
 
-from PyDictionary import PyDictionary
+import random
 from pathlib import Path
 
-INPUT_FILE = Path("/Users/will/Library/Mobile Documents/iCloud~md~obsidian/Documents/Random/words.md")
+from freedictionaryapi.clients.sync_client import DictionaryApiClient
+
+INPUT_FILE = Path(
+    "/Users/will/Library/Mobile Documents/iCloud~md~obsidian/Documents/Random/words.md"
+)
+SECTION_SEPARATOR = "---"
 
 if __name__ == "__main__":
+    with INPUT_FILE.open() as file:
+        # Skip empty lines and remove newlines
+        words = list(word for word in (line.strip() for line in file) if word)
+        # Only use lines in first section of document (until first "---")
+        words = words[: words.index(SECTION_SEPARATOR)]
 
-    dictionary = PyDictionary()
+        word = random.choice(words)
+        print()
+        print(word)
+        print()
+        
 
+        with DictionaryApiClient() as client:
+            parser = client.fetch_parser(word)
+            # print(parser.word.phonetics)
+            for meaning in parser.word.meanings:
+                print(meaning.part_of_speech)
+                for definition in meaning.definitions:
+                    print("-", definition.definition)
+                print()
